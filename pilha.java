ecd49c5fd01290f02e986b154b8168909d930570
felipe-boll <felipe-boll>
2025-11-13 16:58:38 -0300

    arquivos

---
+++ pilha.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class pilha {
+
+    public static HashMap<String, Double> metais = new HashMap<>();
+
+
+    public static void lerArquivo(String file){
+        try{
+            File obj = new File(file);
+            Scanner input = new Scanner(obj);
+
+            while (input.hasNextLine()) {
+                String data = input.nextLine();
+                String valores[] = data.split(" ");
+
+                metais.put(valores[0], Double.parseDouble(valores[1]));
+            }
+
+            input.close();
+
+        } catch(IOException e){
+            System.out.println("Erro: ");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        lerArquivo("Especies.txt");
+    }
+}
--- sla.java
+++
@@ -1,5 +0,0 @@
-public class sla {
-    public static void main(String[] args) {
-        
-    }
-}
